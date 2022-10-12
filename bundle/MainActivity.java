5218f3764ada1a0b4e83a85e933e1244633ac4e2
Jithukrish <94125038+Jithukrish@users.noreply.github.com>
2022-10-12 09:59:34 +0530

    Add files via upload

---
+++ bundle/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+ Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+    }
+
+    public void view(View view) {
+        Intent i=new Intent(this,MainActivity2.class);
+        i.putExtra("name","iris");
+        i.putExtra("name2","ira");
+        startActivity(i);
+    }
+
+
+
+}
